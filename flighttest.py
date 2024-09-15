97b0034595b1c20a576531b79b5d1cec7f235c97
wgreenwood12 <wgreenw@purdue.edu>
2024-09-15 18:06:50 -0400

    Initial commit

--- flighttest.py
+++ flighttest.py
@@ -11,13 +11,14 @@ To load the level first comment out the code below and run the exe file
 Once the level is loaded comment out the simLoadLevel code and uncomment your code
 Run the python script again and you should see the drone takeoff
 """
-
 client.simLoadLevel('Soccer_Field_Easy')
+client.simStartRace()
 
-# client.enableApiControl(vehicle_name="drone_1")
-# client.arm(vehicle_name="drone_1")
+client.enableApiControl(vehicle_name="drone_1")
+client.arm(vehicle_name="drone_1")
 
-# # Async methods returns Future. Call join() to wait for task to complete.
-# client.takeoffAsync(vehicle_name="drone_1").join()
+client.takeoffAsync(vehicle_name="drone_1").join()
+# Async methods returns Future. Call join() to wait for task to complete.
+client.moveToPositionAsync(20, 20, 20, 20, vehicle_name="drone_1").join()
 
-# Code goes here to move the drone 
+# Takeoff from the new starting position
